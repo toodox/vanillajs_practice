8e6d1a97dbd74dbf2c8a5be14054966d9592c3e1
junn <60811775+toodox@users.noreply.github.com>
2020-07-04 14:01:10 +0900

    #2.0 Function

---
+++ index.js
@@ -0,0 +1,7 @@
+    //#2.0 Function
+
+    function sayHello(potato /*argument*/, apple) {
+        console.log("Hello!", potato, "\nyour number is", apple);
+    } //함수 선언
+
+    sayHello("junn", 15); //함수호출
